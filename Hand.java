057010c65bab6d63146c7d8605d8a402b3985069
Zarif Khan <100174834+Nicerice96@users.noreply.github.com>
2023-10-20 14:26:51 -0400

    Made hand!

--- Hand.java
+++ Hand.java
@@ -1,3 +1,17 @@
+import java.util.ArrayList;
+
 public class Hand {
+
+    private ArrayList<Card> cards;
+
+    Hand(ArrayList<Card> cards){}
+
+    public void addCard(Card card){}
+
+    public void removeCard(Card card){}
+
+    public void getCards(ArrayList<Card> cards){}
+
+
     
 }
