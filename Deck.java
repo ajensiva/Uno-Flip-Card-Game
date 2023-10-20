aabf8e7e0329f5ae573d20ff0ad4d9d6965e6ab9
Zarif Khan <100174834+Nicerice96@users.noreply.github.com>
2023-10-20 14:50:41 -0400

    Made hand!

--- Deck.java
+++ Deck.java
@@ -3,4 +3,11 @@ import java.util.Stack;
 public class Deck {
     private Stack<Card> deck;
     private int FULL_DECK = 112;
+
+    Deck(){}
+
+    public Card drawCard(){
+
+        return null;
+    }
 }
