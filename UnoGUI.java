008cfea8ad961cb7ddefd9461e5079f02dd37c93
ajensiva <ajensiva@hotmail.com>
2023-11-06 21:49:51 -0500

    removed duplicate code

--- UnoGUI.java
+++ UnoGUI.java
@@ -137,11 +137,6 @@ public class UnoGUI {
         rootFrame.setSize(FRAME_SIZE_WIDTH, FRAME_SIZE_HEIGHT);
         rootFrame.setVisible(true);
 
-        // clean up root frame
-        rootFrame.pack();
-        rootFrame.setSize(FRAME_SIZE_WIDTH, FRAME_SIZE_HEIGHT);
-        rootFrame.setVisible(true);
-
         for(int i = 0; i < 7; i++){
             addCard();
         }
