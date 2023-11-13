1c977bc756bdd0f292a2e4fe8c00c8e6b6088eff
Arun Karki <arunkarki3629@gmail.com>
2023-11-13 00:37:52 -0500

    borders invisble

--- Controller.java
+++ Controller.java
@@ -1,10 +1,10 @@
 import java.awt.*;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 
-import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
