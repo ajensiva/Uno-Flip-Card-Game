dc00256ad8ed8c1780185b031e307bd57bb7f839
jasonkeah <jasonkeah@cmail.carleton.ca>
2023-11-18 14:18:08 -0500

    Deletion of jargin

--- out/production/Uno-Flip-Card-Game/Uno-Flip-Card-Game/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Card"},{"p":"<Unnamed>","l":"Deck"},{"p":"<Unnamed>","l":"Hand"},{"p":"<Unnamed>","l":"Player"},{"p":"<Unnamed>","l":"Round"},{"p":"<Unnamed>","l":"Uno"}];updateSearchResults();
