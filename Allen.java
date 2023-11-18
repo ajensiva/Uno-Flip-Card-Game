0f56d885b60959846c43e5b0a7f49885d950e0c8
Arun Karki <arunkarki3629@gmail.com>
2023-11-18 13:16:20 -0500

    allen's processes data

--- Allen.java
+++ Allen.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Allen extends Player{
     /**
      * Constructor to create a new player with the given name.
@@ -6,5 +8,26 @@ public class Allen extends Player{
      */
     public Allen(String name) {
         super(name);
+    }
+
+    // this will play the card with the highest value
+    public void allenPlayCard(Round currentRound, Hand hand){
+        /*
+            get the bot's hand to see all which cards are playable first
+            play the card with the most value
+         */
+        ArrayList<Card> allenCards = hand.getHandList();
+        int index = 0;
+
+        for(Card card : allenCards){
+            boolean canPlay = currentRound.checkCard(card, currentRound.discard.peek());
+            // playable
+            if(canPlay && (card.getValue() > allenCards.get(index).getValue())){ // if the value of current value > old then update new highest card
+                index = allenCards.indexOf(card);
+            }
+        }
+        // play the card
+        currentRound.setPlayCardIndex(index);
+        currentRound.cardPlayedLogic();
     }
 }
